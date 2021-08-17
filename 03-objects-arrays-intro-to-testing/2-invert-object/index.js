638bc805b0a09e415acaf34877ec4f69225ddbca
Elena Ilyukhina <elena.ilyukhina@hotmail.com>
2021-08-17 11:31:31 +0100

    Corrections after code review

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,7 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (obj != undefined) {
+  if (obj !== undefined) {
     return Object.fromEntries(Object.entries(obj).map(element => [element[1], element[0]]));
   }
 }
