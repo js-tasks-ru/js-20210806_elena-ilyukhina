a97c89fef6befa959cc260ae941c607a7b1797c0
Elena Ilyukhina <elena.ilyukhina@hotmail.com>
2021-08-24 10:16:54 +0100

    1-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,39 @@
 export default class NotificationMessage {
 
+  static element = NotificationMessage.init();
+  static timeoutId = null;
+
+  static init() {
+    NotificationMessage.element = document.createElement('div');
+    document.body.append(NotificationMessage.element);
+    return NotificationMessage.element;
+  }
+
+  constructor(
+    message = '', {
+      duration = 2000,
+      type = 'success'
+    }) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+  }
+
+  show() {
+    if (NotificationMessage.timeoutId)
+      clearTimeout(NotificationMessage.timeoutId);
+
+    NotificationMessage.element.innerHTML = `
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+    `;
+    NotificationMessage.timeoutId = setTimeout(() => NotificationMessage.element.innerHTML = ``, this.duration);
+  }
 }
