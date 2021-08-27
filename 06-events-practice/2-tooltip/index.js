83d5436e01f84c22580aef1d0e4b504582ff9b1e
Elena Ilyukhina <elena.ilyukhina@hotmail.com>
2021-08-28 00:12:37 +0100

    1-sortable-table-v2
    2-tooltip
    
    2-tooltip
    
    3-double-slider
    
    cleanup

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,68 @@
 class Tooltip {
+  static instance;
 
+  element;
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+    Tooltip.instance = this;
+  }
+
+  render(tooltip = '') {
+    this.element.textContent = tooltip;
+    this.element.style.position = "absolute";
+    this.move(0, 0);
+    document.body.append(this.element);
+  }
+
+  initialize() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `<div class="tooltip">This is tooltip</div>`;
+    this.element = wrapper.firstElementChild;
+
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  onPointerOver = (event) => {
+    if (event.target.dataset.tooltip) {
+      this.render(event.target.dataset.tooltip);
+      event.target.addEventListener('pointermove', this.onPointerMove);
+    }
+  }
+
+  onPointerOut = (event) => {
+    if (event.target.dataset.tooltip) {
+      event.target.removeEventListener('pointermove', this.onPointerMove);
+      this.remove();
+    }
+  }
+
+  onPointerMove = (event) => {
+    this.move(event.clientX, event.clientY);
+  }
+
+  move(x, y) {
+    this.element.style.left = x + 'px';
+    this.element.style.top = y + 'px';
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onPointerOver);
+    document.removeEventListener('pointerout', this.onPointerOut);
+
+    this.remove();
+    this.element = null;
+    Tooltip.instance = null;
+  }
 }
 
 export default Tooltip;
